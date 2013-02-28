9b248a55466679c185608cdf9b70b22bae410c9a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2013-02-27 19:48:42 -0800

    First fully working barebones commit

--- app.rb
+++ app.rb
@@ -1,19 +1,31 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'stripe'
+require 'rack/contrib/jsonp'
+require 'rack/ssl-enforcer'
 
+#Force SSL in production
+if production?
+  use Rack::SslEnforcer
+end
+
+#Set Stripe secret and publishable keys from environment variables
 set :publishable_key, ENV['PUBLISHABLE_KEY']
 set :secret_key, ENV['SECRET_KEY']
-
 Stripe.api_key = settings.secret_key
 
-set :method_override, true
+#Don't know why ip_spoofing is turned off, 
+#this is from https://stripe.com/docs/checkout/guides/sinatra
 set :protection, except: :ip_spoofing
+
+#Rack middleware to detect JSONP
+use Rack::JSONP
 
 get '/' do
   erb :index
 end
 
-post '/charge' do
+get '/donate' do
   # Amount in cents
   @amount = 500
 
@@ -28,8 +40,8 @@ post '/charge' do
     :currency    => 'usd',
     :customer    => customer.id
   )
-
-  erb :charge
+  
+  json charge, :encoder => :to_json
 end
 
 error Stripe::CardError do
